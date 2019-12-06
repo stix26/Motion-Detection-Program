4f5c0ae6a0dcaf051d2119ecb865685391baac52
Stiles Seymens <52355706+stix26@users.noreply.github.com>
2019-12-06 15:02:24 -0600

    Add files via upload

--- motion_detector.py
+++ motion_detector.py
@@ -34,6 +34,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3)
     status_list.append(status)
 
+    status_list=status_list[-2:]
 
     if status_list[-1]==1 and status_list[-2]==0:
         times.append(datetime.now())
